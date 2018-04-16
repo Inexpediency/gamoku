959a35cb38e9d647670d01223bdb6f9fc21b1fa7
lifeStine <vasus714@yandex.ru>
2018-04-16 23:46:02 +0300

    Поправил ошибку частое съезжание клеток

--- client/js/index.js
+++ client/js/index.js
@@ -39,11 +39,12 @@ function invalidateState(gameStarted, isPlayerStep) {
 function updateField(socket, field) {
 	const fieldElement = getGameField();
 	const cells = fieldElement.children;
+	const rowCellsCount = Math.sqrt(field.length);
 	for (let index = 0; index < field.length; ++index) {
 		let cell = cells[index];
 		if (!cell)
 		{
-			cell = createCell();
+			cell = createCell(rowCellsCount, field.length, index);
 			fieldElement.appendChild(cell);
 			cell.addEventListener("click", () => socket.send(index));
 		}
@@ -58,13 +59,31 @@ function invalidateCellsSize(cellsCount) {
 	const fieldElement = getGameField();
 	const rowCellsCount = Math.sqrt(cellsCount);
 	const rowWidth = fieldElement.offsetWidth;
-	const cellSize = rowWidth / rowCellsCount;
+	const cellSize = Math.floor(rowWidth / rowCellsCount);
 	const cells = fieldElement.children;
 	const cellBorder = 1;
 	for (let index = 0; index < cellsCount; ++index) {
 		let cell = cells[index];
-		cell.style.width = `${cellSize - cellBorder * 2}px`;
-		cell.style.height = `${cellSize - cellBorder * 2}px`;
+		let widthBordersCount = 2;
+		let heightBordersCount = 2;
+		if (index < rowCellsCount)
+		{
+			heightBordersCount += 1;
+		}
+		if (index >= cellsCount - rowCellsCount)
+		{
+			heightBordersCount += 1;
+		}
+		if (index % rowCellsCount == 0)
+		{
+			widthBordersCount += 1;
+		}
+		if (index % rowCellsCount == rowCellsCount - 1)
+		{
+			widthBordersCount += 1;
+		}
+		cell.style.width = `${cellSize - cellBorder * widthBordersCount}px`;
+		cell.style.height = `${cellSize - cellBorder * heightBordersCount}px`;
 	}
 }
 
@@ -86,8 +105,24 @@ function getGameField() {
 	return document.getElementById("gameField");
 }
 
-function createCell() {
+function createCell(rowCellsCount, cellsCount, index) {
 	const cell = document.createElement("div");
 	cell.setAttribute("class", "cell");
+	if (index < rowCellsCount)
+	{
+		cell.setAttribute("data-border-top", "true");
+	}
+	if (index >= cellsCount - rowCellsCount)
+	{
+		cell.setAttribute("data-border-bottom", "true");
+	}
+	if (index % rowCellsCount == 0)
+	{
+		cell.setAttribute("data-border-left", "true");
+	}
+	if (index % rowCellsCount == rowCellsCount - 1)
+	{
+		cell.setAttribute("data-border-right", "true");
+	}
 	return cell;
 }
